aa578b7fbdec525968f511efff1a3fc72e584dd4
Bisan09 <147491062+Bisan09@users.noreply.github.com>
2025-04-01 12:28:32 +0200

    Primer Ejercicio de Math

---
+++ Math.js
@@ -0,0 +1,15 @@
+/*Genera un número aleatorio entre 0 y 100 redondeando y muéstralo por consola*/
+
+let numAleatorio = Math.round(Math.random() * 100);
+console.log(numAleatorio);
+
+/*Muestra por consola el valor de PI.*/
+
+
+
+/*Genera un número aleatorio entre 5 y 10 redondeando y muéstralo por consola. Para  este caso lo recomendado es utilizar la fórmula (Math.random()* (max-min)+min).*/
+
+
+
+/*Comprueba el funcionamiento del método sign().*/
+
