3ac8533e4072f641ec53087f0c9d24ed0a0a9fe1
Bisan09 <147491062+Bisan09@users.noreply.github.com>
2025-04-01 12:29:45 +0200

    Enunciado 2

--- Math.js
+++ Math.js
@@ -5,7 +5,7 @@ console.log(numAleatorio);
 
 /*Muestra por consola el valor de PI.*/
 
-
+console.log(Math.PI);
 
 /*Genera un número aleatorio entre 5 y 10 redondeando y muéstralo por consola. Para  este caso lo recomendado es utilizar la fórmula (Math.random()* (max-min)+min).*/
 
